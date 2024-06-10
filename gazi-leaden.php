429633e46e1fa96ae69cf1bc672197c73b920127
Gazi Akter <gaziakterdev@gmail.com>
2024-06-10 23:31:34 +0600

    initial setup plugin

---
+++ gazi-leaden.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Gazi Leaden
+ * Description: A lead generation plugin.
+ * Plugin URI:  https://owndevs.com/plugin/gazi-leaden
+ * Version:     1.0.0
+ * Author:      Gazi Akter
+ * Author URI:  https://gaziakter.com/
+ * Text Domain: gazi-leaden
+ * Elementor tested up to: 3.5.0
+ * Elementor Pro tested up to: 3.5.0
+ */
